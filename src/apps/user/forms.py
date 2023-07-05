364494afab851e6c3a82505313f62940afb1bec9
sudhirsth <sudhirsth03@gmail.com>
2023-07-05 13:36:13 +0545

    auth update

--- src/apps/user/forms.py
+++ src/apps/user/forms.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from .models import User, UserProfile
+from django.contrib.auth.models import Group
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -70,3 +71,37 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("email", "password1", "password2")
+
+
+class CustomCheckboxSelectMultiple(forms.CheckboxSelectMultiple):
+    template_name = 'customcheckbox.html'
+
+
+class UserForm(forms.ModelForm):
+    groups = forms.ModelMultipleChoiceField(
+        queryset=Group.objects.all()        
+    )
+
+    email = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "type": "email",
+                "placeholder": "john@domain.com",
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'groups']
+        widgets = {'password': forms.PasswordInput}
--- src/apps/user/urls.py
+++ src/apps/user/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from .views import login_user, logout_view, register_user, user_list
+from .views import login_user, logout_view, register_user, user_list, user_groups, create_user, delete_user
 from django.contrib.auth.views import LogoutView
 
 
@@ -10,4 +10,9 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('register/', register_user, name='register'),
     path('users/', user_list.as_view(), name="user_list"),
+    path('users/groups/<id>', user_groups.as_view(), name="user_groups"),
+    path('users/create/', create_user.as_view(), name="user_create"),
+    path('users/delete/<int:id>/', delete_user.as_view(), name='delete_user')
+
+
 ]
